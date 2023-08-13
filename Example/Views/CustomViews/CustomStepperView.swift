1c192abd03f60a5839484ec0644d98e6bcb454d7
MAHESHWARAN <maheshwaran@eam360.com>
2023-08-13 20:26:06 +0530

    CustomStepperView

---
+++ Example/Views/CustomViews/CustomStepperView.swift
@@ -0,0 +1,40 @@
+//
+//  CustomStepperView.swift
+//  Examples
+//
+//  Created by MAHESHWARAN on 06/08/23.
+//
+
+import SwiftUI
+
+struct CustomStepperView: View {
+  
+  @State private var sleepAmount = 8.0
+  
+  var body: some View {
+    TabView {
+      stepperView
+      stepperViewOne
+    }
+    .tabViewStyle(.page)
+    .indexViewStyle(.page(backgroundDisplayMode: .always))
+  }
+  
+  private var stepperView: some View {
+    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+      .padding()
+  }
+  
+  private var stepperViewOne: some View {
+    Stepper("\(sleepAmount) hours", value: $sleepAmount, in: 4...12)
+      .padding()
+  }
+  
+  
+}
+
+struct CustomStepperView_Previews: PreviewProvider {
+  static var previews: some View {
+    CustomStepperView()
+  }
+}
