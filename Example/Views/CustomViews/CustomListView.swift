7526ec16b5b1cf3d7312b70fee5a23b43c400eb5
MAHESHWARAN <maheshwaran@eam360.com>
2023-09-01 09:51:19 +0530

    CustomListView  - Updated examples for listRow

--- Example/Views/CustomViews/CustomListView.swift
+++ Example/Views/CustomViews/CustomListView.swift
@@ -12,6 +12,10 @@ struct CustomListView: View {
   var body: some View {
     TabView {
       selectionListView
+      customListBackgroundView
+      customListRowBackgroundView
+      customHeightListView
+      customHeaderListView
       hideRightChevronInlistView
       expandableListView
       listViewTint
@@ -96,6 +100,80 @@ struct CustomListView: View {
         Label("Row \(i)", systemImage: "\(i).circle")
             .listItemTint(i.isMultiple(of: 2) ? .red : .green)
     }
+  }
+  
+  // MARK: - Custom Height
+  
+  private var customHeightListView: some View {
+    
+    List(0..<2, id: \.self) { item in
+      Section {
+        titleView(item)
+      } header: {
+        Text("Header")
+      }
+      .headerProminence(.increased)
+      
+      Section {
+        titleView(item)
+      } header: {
+        Text("Body")
+      }
+    }
+    .listStyle(.plain)
+    .environment(\.defaultMinListRowHeight, 80)
+    .environment(\.defaultMinListHeaderHeight, 20)
+  }
+  
+  // MARK: - Custom Header
+  
+  private var customHeaderListView: some View {
+    
+    List(0..<1, id: \.self) { item in
+      Section {
+        titleView(item)
+      } header: {
+        Text("Header")
+      }
+      .headerProminence(.increased)
+      
+      Section {
+        titleView(item)
+      } header: {
+        Text("Body")
+          .font(.title2)
+          .foregroundColor(.red)
+      }
+    }
+    .listStyle(.plain)
+    .environment(\.defaultMinListHeaderHeight, 20)
+  }
+  
+  // MARK: - Custom List View Background
+  
+  private var customListBackgroundView: some View {
+    List(0..<3, id: \.self) { item in
+      titleView(item)
+    }
+    .scrollContentBackground(.hidden)
+    .background(
+      LinearGradient(colors: [.white, .blue], startPoint: .top, endPoint: .bottom)
+        .blur(radius: 8)
+    )
+  }
+  
+  // MARK: - Custom List Row Background
+  
+  private var customListRowBackgroundView: some View {
+    List(0..<3, id: \.self) { item in
+      Text(String(describing: item))
+        .listRowSeparator(.hidden)
+        .listRowBackground(
+          LinearGradient(colors: [.white, .red], startPoint: .leading, endPoint: .trailing))
+        .padding(.vertical, 4)
+          
+    }
+    .listStyle(.plain)
   }
 }
 
