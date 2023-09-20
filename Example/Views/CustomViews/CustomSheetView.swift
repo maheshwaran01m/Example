17168606a30e0c509bf841787b6f8dcae18ff8d9
MAHESHWARAN <maheshwaran@eam360.com>
2023-09-20 11:01:08 +0530

    CustomSheetView  - Handled minHeight

--- Example/Views/CustomViews/CustomSheetView.swift
+++ Example/Views/CustomViews/CustomSheetView.swift
@@ -16,6 +16,7 @@ struct CustomSheetView: View {
     }
     .tabViewStyle(.page(indexDisplayMode: .always))
     .indexViewStyle(.page(backgroundDisplayMode: .always))
+    .ignoresSafeArea(.container, edges: .bottom)
   }
   // MARK: - View Height
   
@@ -47,6 +48,7 @@ struct CustomSheetView: View {
       }
       .bottomSheetView($showSheet) {
         Text("Hello")
+          .frame(height: 100)
       }
   }
 }
@@ -61,54 +63,44 @@ struct BottomSheetView<Content: View>: View {
   
   @Binding var isPresented: Bool
   @GestureState private var translation: CGFloat = 0
+  @State private var minHeight: CGFloat = 0
   
   private var content: Content
   private let maxHeight: BottomSheetHeight
-  private let minHeight: BottomSheetHeight
   
   init(isPresented: Binding<Bool>,
-       minHeight: BottomSheetHeight = .height(100),
        maxHeight: BottomSheetHeight = .fullScreen,
        content: @escaping () -> Content) {
     _isPresented = isPresented
     self.content = content()
     self.maxHeight = maxHeight
-    self.minHeight = minHeight
   }
   
   var body: some View {
     GeometryReader { proxy in
-      VStack(spacing: 0) {
-        handle(for: proxy)
-        contentView(for: proxy)
+      VStack(spacing: 8) {
+        handleView
+        content
       }
+      .getHeight($minHeight)
       .frame(width: proxy.size.width, height: maxHeight(in: proxy), alignment: .top)
-      .background(.orange.opacity(0.1))
+      .background(Color.gray.opacity(0.1))
       .clipShape(RoundedRectangle(cornerRadius: 16))
-      .frame(height: proxy.size.height + proxy.safeAreaInsets.bottom, 
-             alignment: .bottom)
       .offset(y: max(offset(for: proxy) + translation, 0))
+      .frame(height: proxy.size.height + proxy.safeAreaInsets.bottom,
+             alignment: .bottom)
+      .gesture(dragGesture(proxy))
+      .ignoresSafeArea(.container, edges: .bottom)
       .transition(.move(edge: .bottom))
     }
   }
   
-  
-  func contentView(for proxy: GeometryProxy) -> some View {
-    content
-      .padding(.bottom, proxy.safeAreaInsets.bottom)
-  }
-  
-  func handle(for proxy: GeometryProxy) -> some View {
-    VStack(spacing: 10) {
-      handleView
-      Divider()
-        .frame(maxWidth: .infinity)
-    }
-    .background(Color.blue.opacity(0.1))
-    .onTapGesture {
-      isPresented.toggle()
-    }
-    .gesture(dragGesture(proxy))
+  private var handleView: some View {
+    Rectangle()
+      .fill(Color.gray.opacity(0.8))
+      .cornerRadius(45)
+      .frame(width: 68, height: 4)
+      .padding(.top, 8)
   }
   
   private func dragGesture(_ proxy: GeometryProxy) -> some Gesture {
@@ -117,26 +109,13 @@ struct BottomSheetView<Content: View>: View {
         state = value.translation.height
       }
       .onEnded { value in
-        withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.9)) {
-          let height = abs(value.translation.height)
-          let ratio: CGFloat = 0.25
-          let distance = maxHeight(in: proxy) * ratio
-          let apply = height > distance
-          guard apply else { return }
-          isPresented = value.translation.height < 0
-        }
+        let height = abs(value.translation.height)
+        let distance = maxHeight(in: proxy) * CGFloat(0.25)
+        let apply = height > distance
+        guard apply else { return }
+        isPresented = value.translation.height < 0
       }
   }
-  
-  
-  private var handleView: some View {
-    Rectangle()
-      .fill(Color.gray.opacity(0.3))
-      .cornerRadius(45)
-      .frame(width: 68, height: 4)
-      .padding(.top, 8)
-  }
-  
   
   func height(of height: BottomSheetHeight, in proxy: GeometryProxy) -> CGFloat {
     switch height {
@@ -149,16 +128,12 @@ struct BottomSheetView<Content: View>: View {
     }
   }
   
-  func minHeight(in geo: GeometryProxy) -> CGFloat {
-      height(of: minHeight, in: geo)
-  }
-  
   func maxHeight(in geo: GeometryProxy) -> CGFloat {
-      height(of: maxHeight, in: geo)
+    height(of: maxHeight, in: geo)
   }
   
   func offset(for proxy: GeometryProxy) -> CGFloat {
-    isPresented ? 0 : maxHeight(in: proxy) - minHeight(in: proxy)
+    isPresented ? 0 : maxHeight(in: proxy) - minHeight
   }
   
   enum BottomSheetHeight {
@@ -173,15 +148,17 @@ extension View {
   @ViewBuilder
   func bottomSheetView<Content: View>(
     _ isPresented: Binding<Bool>,
-    minHeight: BottomSheetView<Content>.BottomSheetHeight = .height(250),
-    maxHeight: BottomSheetView<Content>.BottomSheetHeight = .percentage(0.8),
+    maxHeight: BottomSheetView<Content>.BottomSheetHeight = .percentage(0.9),
     content: @escaping () -> Content) -> some View {
-        ZStack {
-          self
-          BottomSheetView(isPresented: isPresented,
-                      minHeight: minHeight,
-                      maxHeight: maxHeight,
-                      content: content)
-        }
+      
+      ZStack {
+        self
+          .onTapGesture {
+            isPresented.wrappedValue = false
+          }
+        BottomSheetView(isPresented: isPresented,
+                        maxHeight: maxHeight,
+                        content: content)
+      }
     }
 }
