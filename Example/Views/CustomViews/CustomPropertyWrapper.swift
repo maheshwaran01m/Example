6c9433ec8a4532bb8b08d0e78995ff61fd70d817
MAHESHWARAN <maheshwaran@eam360.com>
2023-10-28 18:58:54 +0530

    CustomPropertyWrapper  - Created examples

--- Example/Views/CustomViews/CustomPropertyWrapper.swift
+++ Example/Views/CustomViews/CustomPropertyWrapper.swift
@@ -6,15 +6,79 @@
 //
 
 import SwiftUI
+import Combine
 
 struct CustomPropertyWrapper: View {
-  @StringFormat(format: .lowercased) private var text = ""
   
   var body: some View {
+    TabView {
+      fileManagerPropertyView
+      stringFormatView
+      capitalizedView
+    }
+    .tabViewStyle(.page)
+    .indexViewStyle(.page)
+  }
+  
+  // MARK: - StringFormat
+  
+  @StringFormat(format: .lowercased) private var text = ""
+  
+  var stringFormatView: some View {
     VStack {
       Text(text)
       TextField("Enter", text: $text)
     }
+    .padding(.horizontal)
+  }
+  
+  // MARK: - FileManagerProperty
+  
+  //  @FileManagerProperty("fileManagerProperty") private var user: User?
+  @FileManagerProperty(\.key) private var user: User?
+  
+  var fileManagerPropertyView: some View {
+    VStack(spacing: 10) {
+      Text(user?.title ?? "")
+      Text(user?.subtitle ?? "")
+      
+      Button("Save") {
+        user = .init(title: "title \(Int.random(in: 0...20))",
+                     subtitle: "subtitle \(Int.random(in: 0...20))")
+      }
+      .buttonStyle(.borderedProminent)
+      .padding()
+    }
+    .onReceive(user.publisher) { value in
+      print("User: \(value.title)")
+    }
+    .task {
+      for await newValue in user.publisher.values {
+        print("User: \(newValue.title)")
+      }
+    }
+  }
+  
+  // MARK: - Capitalized
+  
+  @Capitalized private var value: String = "hello"
+  
+  var capitalizedView: some View {
+    VStack {
+      Button(value) {
+        value = "NewValue \(Int.random(in: 0...20))"
+      }
+    }
+  }
+  
+  struct User: Codable {
+    var title: String
+    var subtitle: String
+  }
+  
+  struct Keys {
+    let key = "userProfile"
+    let type: User.Type
   }
 }
 
@@ -64,4 +128,136 @@ struct StringFormat: DynamicProperty {
       return string
     }
   }
+}
+
+@propertyWrapper
+struct Capitalized: DynamicProperty {
+  @State private var value: String
+  
+  init(wrappedValue: String) {
+    self.value = wrappedValue
+  }
+  
+  var wrappedValue: String {
+    get {
+      value
+    } nonmutating set {
+      value = newValue
+    }
+  }
+  
+  var projectedValue: Binding<String> {
+    .init {
+      wrappedValue
+    } set: {
+      wrappedValue = $0
+    }
+  }
+}
+
+@propertyWrapper
+struct FileManagerProperty<T: Codable>: DynamicProperty {
+  
+  @State private var value: T?
+  let key: String
+  
+  var wrappedValue: T? {
+    get { value } nonmutating set { save(newValue) }
+  }
+  
+  // MARK: - ProjectedValue
+  
+  var projectedValue: Binding<T?> {
+    .init { wrappedValue } set: { wrappedValue = $0 }
+  }
+  
+  // MARK: - Init
+  
+  init(wrappedValue: T? = nil, _ key: String) {
+    self.key = key
+    
+    do {
+      let filePath = URL.documentsDirectory.appending(path: "\(key).txt")
+      
+      let data = try Data(contentsOf: filePath)
+      let value = try JSONDecoder().decode(T.self, from: data)
+      currentPublisher = .init(value)
+      _value = .init(initialValue: value)
+    } catch {
+      print("Error while reading document, reason: \(error.localizedDescription)")
+      currentPublisher = .init(nil)
+      _value = .init(initialValue: wrappedValue ?? nil)
+    }
+  }
+  
+  init(_ key: KeyPath<FileValues, FileKeyPath<T>>) {
+    self.key = FileManagerProperty<T>.FileValues()[keyPath: key].key
+    
+    do {
+      let filePath = URL.documentsDirectory.appending(path: "\(key).txt")
+      
+      let data = try Data(contentsOf: filePath)
+      let value = try JSONDecoder().decode(T.self, from: data)
+      currentPublisher = .init(value)
+      _value = .init(initialValue: value)
+    } catch {
+      print("Error while reading document, reason: \(error.localizedDescription)")
+      currentPublisher = .init(nil)
+      _value = .init(initialValue: wrappedValue ?? nil)
+    }
+  }
+  
+  struct FileKeyPath<K: Codable> {
+    let key: String
+    let type: K.Type
+  }
+  
+  struct FileValues {
+    init() {}
+    
+    let key = FileKeyPath(key: "user_profile", type: T.self)
+  }
+  
+  // MARK: - Custom Methods
+  
+  private func save(_ newValue: T?) {
+    do {
+      let data = try JSONEncoder().encode(newValue)
+      let filePath = URL.documentsDirectory.appending(path: "\(key).txt")
+      
+      try data.write(to: filePath)
+      value = newValue
+      currentPublisher.send(value)
+    } catch {
+      print("Error while saving document, reason: \(error.localizedDescription)")
+    }
+  }
+  
+  // MARK: - Combine
+  
+  private let currentPublisher: CurrentValueSubject<T?, Never>
+  
+  public var publisher: CurrentValueSubject<T?, Never> {
+    currentPublisher
+  }
+  
+  public var stream: AsyncPublisher<CurrentValueSubject<T?, Never>> {
+    currentPublisher.values
+  }
+  
+  /*
+   struct ProjectedValue<K: Codable> {
+   let binding: Binding<K?>
+   let publisher: CurrentValueSubject<K?, Never>
+   
+   var stream: AsyncPublisher<CurrentValueSubject<K?, Never>> {
+   publisher.values
+   }
+   }
+   
+   var projectedValue: ProjectedValue<T> {
+   .init(binding: .init { wrappedValue } set: { wrappedValue = $0 },
+   publisher: currentPublisher)
+   }
+   */
 }
