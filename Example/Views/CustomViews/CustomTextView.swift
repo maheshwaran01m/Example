3afe237e061977b529418bfbb9d823daf745458c
MAHESHWARAN <maheshwaran@eam360.com>
2023-11-03 19:10:33 +0530

    CustomTextView  - Updated example

--- Example/Views/CustomViews/CustomTextView.swift
+++ Example/Views/CustomViews/CustomTextView.swift
@@ -10,10 +10,17 @@ import SwiftUI
 struct CustomTextView: View {
   
   var body: some View {
-    VStack {
-      titleView
-      titleViewOne
+    TabView {
+      VStack {
+        titleView
+        titleViewOne
+      }
+      VStack(spacing: 40) {
+        rectangleView
+        roundedRectangleView
+      }
     }
+    .tabViewStyle(.page)
   }
   
   private var titleView: some View {
@@ -28,6 +35,35 @@ struct CustomTextView: View {
       .padding(10)
       .foregroundColor(Color.primary)
       .background(Color.gray.opacity(0.2), in: Capsule())
+  }
+  
+  private var rectangleView: some View {
+    Text("Hello World")
+      .frame(width: 275, height: 275)
+      .overlay(
+        Rectangle()
+          .stroke(.primary, style: .init(
+            lineWidth: 3,
+            lineCap: .round,
+            lineJoin: .round,
+            dash: [60, 215],
+            dashPhase: 25))
+      )
+  }
+  
+  private var roundedRectangleView: some View {
+    Text("Hello World")
+      .frame(width: 320, height: 320)
+      .overlay(
+        RoundedRectangle(cornerRadius: 16, style: .circular)
+          .stroke(.primary, style: .init(
+            lineWidth: 3,
+            lineCap: .round,
+            lineJoin: .round,
+            dash: [65, 255],
+            dashPhase: 210) // -110
+          )
+      ) // 320*4 = 1280 -> [65*4 + 255*4]
   }
 }
 
