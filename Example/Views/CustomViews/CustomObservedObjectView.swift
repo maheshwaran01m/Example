a60cc4628d98ab224371b02e027d9e324fae8168
MAHESHWARAN <maheshwaran@eam360.com>
2023-12-13 06:41:41 +0530

    CustomObservedObjectView  - Handled nested viewModel

--- Example/Views/CustomViews/CustomObservedObjectView.swift
+++ Example/Views/CustomViews/CustomObservedObjectView.swift
@@ -94,18 +94,11 @@ struct CustomObservedObjectView2: View {
 fileprivate class CustomObservedObjectViewModel1: ObservableObject {
   
   @Published var text: String?
-  @Published var secondVM: CustomObservedObjectViewModel2
+  @Republished var secondVM: CustomObservedObjectViewModel2
   
   init(_ text: String? = "Example") {
     self.text = text
     self.secondVM = CustomObservedObjectViewModel2(text)
-    setupNestedObject()
-  }
-  
-  private var cancelBag = Set<AnyCancellable>()
-  
-  func setupNestedObject() {
-    secondVM.republish(self, cancelBag: &cancelBag)
   }
 }
 
@@ -117,18 +110,52 @@ class CustomObservedObjectViewModel2: ObservableObject {
   }
 }
 
-extension ObservableObject where Self.ObjectWillChangePublisher == ObservableObjectPublisher  {
+@propertyWrapper
+struct Republished<Obj: ObservableObject> {
+  private var storage: Obj
+  private var subscription: AnyCancellable? = nil
   
-  func republish<T: ObservableObject>(
-    _ observableObject: T,
-    cancelBag: inout Set<AnyCancellable>)
-  where T.ObjectWillChangePublisher == ObservableObjectPublisher  {
-    
-    objectWillChange
-      .receive(on: DispatchQueue.main)
-      .sink { [weak observableObject] _ in
-        observableObject?.objectWillChange.send()
+  init(wrappedValue: Obj) {
+    self.storage = wrappedValue
+  }
+  
+  @available(*, unavailable, message: "Republished can only be used inside reference types that conform to ObservableObject")
+  var wrappedValue: Obj {
+    get { fatalError() }
+    set { fatalError() }
+  }
+  
+  static subscript<EnclosingSelf: ObservableObject>(
+    _enclosingInstance enclosing: EnclosingSelf,
+    wrapped wrappedKeyPath: ReferenceWritableKeyPath<EnclosingSelf, Obj>,
+    storage storageKeyPath: ReferenceWritableKeyPath<EnclosingSelf, Republished<Obj>>
+  ) -> Obj where EnclosingSelf.ObjectWillChangePublisher == ObservableObjectPublisher {
+    get {
+      // Connect child's objectWillChange to parent's objectWillChange.
+      if enclosing[keyPath: storageKeyPath].subscription == nil {
+        let parentPublisher = enclosing.objectWillChange
+        let childPublisher = enclosing[keyPath: storageKeyPath].storage.objectWillChange
+        enclosing[keyPath: storageKeyPath].subscription = childPublisher.sink { _ in
+          parentPublisher.send()
+        }
       }
-      .store(in: &cancelBag)
+      return enclosing[keyPath: storageKeyPath].storage
+    }
+    set {
+      // Cancel old child's connection to parent.
+      if enclosing[keyPath: storageKeyPath].subscription != nil {
+        enclosing[keyPath: storageKeyPath].subscription = nil
+      }
+      enclosing[keyPath: storageKeyPath].storage = newValue
+      
+      // Connect new child's objectWillChange to parent's objectWillChange.
+      let parentPublisher = enclosing.objectWillChange
+      let childPublisher = newValue.objectWillChange
+      enclosing[keyPath: storageKeyPath].subscription = childPublisher.sink { _ in
+        parentPublisher.send()
+      }
+      // Must tell parent explicitly that it has changed.
+      parentPublisher.send()
+    }
   }
 }
